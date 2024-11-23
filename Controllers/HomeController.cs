ab208c1b3573c703bf1b0e2c870d9f2285ade0d4
reypenajr <99457307+reypenajr@users.noreply.github.com>
2024-11-22 21:54:44 -0500

    Added Reviews Model/Controller/Pages and Homepage with non functional search

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,49 @@
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
 using Group8_BrarPena.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.Extensions.Logging;
 
 namespace Group8_BrarPena.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IAmazonDynamoDB _dynamoDbClient;
+        private readonly Table _coursesTable;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IAmazonDynamoDB dynamoDbClient)
         {
             _logger = logger;
+            _dynamoDbClient = dynamoDbClient;
+            _coursesTable = Table.LoadTable(_dynamoDbClient, "Courses");
         }
 
-        public IActionResult Index()
+        // GET: Home/Index
+        public async Task<IActionResult> Index(string? course)
         {
-            return View();
+            var scanFilter = new ScanFilter();
+            if (!string.IsNullOrEmpty(course))
+            {
+                scanFilter.AddCondition("CourseCode", ScanOperator.Contains, course);
+            }
+
+            var search = _coursesTable.Scan(scanFilter);
+            var documents = await search.GetNextSetAsync();
+
+            var courses = documents.Select(doc => new Course
+            {
+                CourseId = doc["CourseId"],
+                CourseCode = doc["CourseCode"],
+                CourseYearSem = doc["CourseYearSem"],
+                ProgramCode = doc["ProgramCode"],
+                Term = doc["Term"]
+            }).ToList();
+
+            return View(courses);
         }
 
         public IActionResult Privacy()
